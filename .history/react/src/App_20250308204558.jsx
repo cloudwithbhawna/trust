9e82c01ac8a7b1809ee17d3683d749840d6ac8a8
Bhawna Vishwakarma <159766023+cloudwithbhawna@users.noreply.github.com>
2025-03-15 20:14:29 +0530

    Delete .history directory

--- .history/react/src/App_20250308204558.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const App = () => {
-  return (
-   
-
-export default App;
